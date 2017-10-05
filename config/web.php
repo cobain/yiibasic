9db2b14d6a12e72ed11d47d82c0901a15db0b3e8
ht zheng <zhtsuc@gmail.com>
2017-10-05 22:47:06 +0800

    save session to be redis

--- config/web.php
+++ config/web.php
@@ -67,15 +67,23 @@ $config = [
 		        // ...
 	        ],
         ],
+//        'session' => [
+//	        'class' => 'yii\web\DbSession',
+//
+//	        // Set the following if you want to use DB component other than
+//	        // default 'db'.
+//	        // 'db' => 'mydb',
+//
+//	        // To override default session table, set the following
+//	        // 'sessionTable' => 'my_session',
+//        ],
         'session' => [
-	        'class' => 'yii\web\DbSession',
-
-	        // Set the following if you want to use DB component other than
-	        // default 'db'.
-	        // 'db' => 'mydb',
-
-	        // To override default session table, set the following
-	        // 'sessionTable' => 'my_session',
+	        'class' => 'yii\redis\Session',
+	        'redis' => [
+		        'hostname' => 'localhost',
+		        'port' => 6379,
+		        'database' => 0,
+	        ]
         ],
     ],
     'params' => $params,
